467f6bcf9a333e42f5c5483f303365d5813a6c8b
Deron Nicholson <deron_nicholson@sagicor.com>
2023-08-09 13:13:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def recommendProductsByRating():
 
        
         
-        recs=recs.withColumn("product_id_index", df["product_id_index"].cast(IntegerType()))
+        recs=recs.withColumn("product_id_index", recs["product_id_index"].cast(IntegerType()))
 
         app.logger.error('\n Before flatmap. Columns: {0}'.format(''.join(recs.columns)))
         product_id_list=recs.select("product_id_index").rdd.flatMap(lambda x: x).collect()
