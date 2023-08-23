7d42252c661f99f0250e0de79712c4cdd1b21608
Deron Nicholson <deron_nicholson@sagicor.com>
2023-08-23 02:33:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import re
 from textblob import TextBlob
 from pyspark.sql.functions import *
 import logging
-#from transformers.lemmatizer import Lemmatizer
+from transformers.lemmatizer import Lemmatizer
 from pyspark.ml import Transformer
 from pyspark.ml.param.shared import HasInputCol, HasOutputCol,TypeConverters
 from pyspark.ml.util import DefaultParamsWritable, DefaultParamsReadable
@@ -409,5 +409,6 @@ def recommendProductsByRating():
             connection.close()
     
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=8080)
+    app.run()
